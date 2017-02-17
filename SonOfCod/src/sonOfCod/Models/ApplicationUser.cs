ef4c5697aff82e78316a10c797294b31ce05b260
Brian Pritt <brianpritt@gmail.com>
2017-02-17 09:31:01 -0800

    initial commit, add authentication and models

---
+++ SonOfCod/src/sonOfCod/Models/ApplicationUser.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+namespace sonOfCod.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+
+    }
+}
---
+++ SonOfCod/src/sonOfCod/Models/MailingList.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace sonOfCod.Models
+{
+    [Table("MailingLists")]
+    public class MailingList
+    {
+        [Key]
+        public int CustomerId { get; set; }
+        public int CustomerFirstName { get; set; }
+        public int CustomerLastName { get; set; }
+        public int CustomerEmail { get; set; }
+
+        public MailingList() { }
+    }
+}
